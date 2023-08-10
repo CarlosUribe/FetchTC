653c4a7574a94c08489ca1b5f8213d7f3cea7488
Carlos <scadtonia@hotmail.com>
2023-08-10 17:48:27 -0600

    Initial Commit

---
+++ FetchTC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchTC
+//
+//  Created by Carlos Uribe on 10/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
